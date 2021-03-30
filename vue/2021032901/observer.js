66df4def92d870761056af6840b38b26c86330db
2017hwx <429794965@qq.com>
2021-03-30 23:00:10 +0800

    2021033003

--- vue/2021032901/observer.js
+++ vue/2021032901/observer.js
@@ -1,3 +1,20 @@
+const defineReactive = (obj, prop, val) => {
+    Object.defineProperty(obj, prop, {
+        configurable: false,
+        enumerable: true,
+        writable: true,
+        get: function () {
+            return val;
+        },
+        set: function (newval) {
+            if (val === newval) {
+                return;
+            }
+            val = newval;
+        }
+    })
+}
+
 class Observer {
     constructor(value) {
         this.value = value;
